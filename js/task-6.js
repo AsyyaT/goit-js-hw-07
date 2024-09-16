e533b34dedeebbf6ee48485aa21b9d9be81c706f
anastasiia <aizatsana15@gmail.com>
2024-09-16 14:37:32 +0300

    HW-7 update createBoxes function

--- js/task-6.js
+++ js/task-6.js
@@ -6,16 +6,22 @@ function getRandomHexColor() {
 
 function createBoxes(amount) {
     const boxesContainer = document.getElementById('boxes');
-    boxesContainer.innerHTML = '';
+    let markup = '';
+    let size = 30;
 
     for (let i = 0; i < amount; i++) {
-        const size = 30 + i * 10;
-        const box = document.createElement('div');
-        box.style.width = `${size}px`;
-        box.style.height = `${size}px`;
-        box.style.backgroundColor = getRandomHexColor();
-        boxesContainer.appendChild(box);
+        markup += `
+            <div style="
+                width: ${size}px;
+                height: ${size}px;
+                background-color: ${getRandomHexColor()};
+                margin: 5px;
+            "></div>
+        `;
+        size += 10;
     }
+
+    boxesContainer.innerHTML = markup; 
 }
 
 function destroyBoxes() {
